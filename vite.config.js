abba8d7228086c0d42ae79c452e42df294fd6a5f
AnkitD-3062 <deyankit3062@gmail.com>
2026-04-08 22:34:46 +0530

    Set up GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  server: { port: 3000 }
-})
+  base: command === 'build' ? '/snapbooth/' : '/',
+  server: { port: 3000 },
+}))
